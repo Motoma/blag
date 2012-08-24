7d1cbdfead52499275c0e181ccaf1db48af73b71
Chris Gilbert <Chris-Gilbert@idexx.com>
2012-08-24 17:40:07 -0400

    Fixed about me links. Added google and piwik tracker code.

--- data/about.py
+++ data/about.py
@@ -1,16 +1,16 @@
 title = "Christopher Gilbert, {App,Sec,Web} Developer"
 data  = """
-<p>I am a newly employed Configuration Manager at IDEXX Laboratories in Westbrook, Maine. IDEXX specializes in development of information technology-based products and services for veterinarians. The company is also a global leader in providing livestock and poultry diagnostic tests as well as tests for the quality and safety of water and milk.</p>
+<p>I am a newly employed Configuration Manager at <a href="http://www.idexx.com/">IDEXX Laboratories</a> in Westbrook, Maine. IDEXX specializes in development of information technology-based products and services for veterinarians. The company is also a global leader in providing livestock and poultry diagnostic tests as well as tests for the quality and safety of water and milk.</p>
 
-<p>I develop in Java, PHP, and Python.</p>
+<p>I develop in <a href="http://java.com/">Java</a>, <a href="http://www.php.net/">PHP</a>, and <a href="http://python.org/">Python</a>.</p>
 
-<p>I write the semi-topical blog MotomaSTYLE, focusing on software development and targeted at system administrators.</p>
+<p>I write the semi-topical blog <a href="http://motoma.io">Motoma.io</a>, focusing on software development and targeted at system administrators.</p>
 
-<p>I am the creator of PyLoris, a scriptable, protocol-agnostic denial of service attack framework.</p>
+<p>I am the creator of <a href="/pyloris/">PyLoris</a>, a scriptable, protocol-agnostic denial of service attack framework.</p>
 
-<p>I am a maintainer for socksipy-branch, a SOCKS library for Python developers featuring native module wrapping.</p>
+<p>I am a maintainer for <a href="http://code.google.com/p/socksipy-branch">socksipy-branch</a>, a SOCKS library for Python developers featuring native module wrapping.</p>
 
-<p>I developed a proof-of-concept network RAID architecture, which was published on Hack a Day.</p>
+<p>I developed a <a href="/network-raid-storage-proof-of-concept/">proof-of-concept</a> network RAID architecture, which was <a href="http://hackaday.com/2007/12/29/raid-your-network-file-shares/">published on Hack a Day</a>.</p>
 
-<p>I have worked in the insurance, communications, and new-media industries. I have developed under ADA/HIPAA/FERPA requirements and audited other companies that operate under these regulations.</p>
+<p>I have worked in the insurance, communications, and new-media industries. I have developed under <a href="http://en.wikipedia.org/wiki/Americans_with_Disabilities_Act_of_1990">ADA</a>/<a href="http://en.wikipedia.org/wiki/Health_Insurance_Portability_and_Accountability_Act">HIPAA</a>/<a href="http://en.wikipedia.org/wiki/Family_Educational_Rights_and_Privacy_Act">FERPA</a> requirements and audited other companies that operate under these regulations.</p>
 """
