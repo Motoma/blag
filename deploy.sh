f97bd86250518bff176fae23ff8113612bfc88a9
Chris Gilbert <Chris-Gilbert@idexx.com>
2012-08-28 13:04:53 -0400

    2012-08-28T13:04-0400 - Automatic commit from build

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
-tar -czvf static.tgz static
+tar -czf static.tgz static
 cd www
-tar -czvf ../www.tgz *
+tar -czf ../www.tgz *
 cd ..
 scp *.tgz web1.motoma.io:
 #ssh web1.motoma.io -C "mkdir -p ~/backup; mv /var/www/motoma.io ~/backup/`date -Iminutes`; mkdir -p /var/www/motoma.io; tar -zxvf www.tgz -C /var/www/motoma.io; tar -zxvf static.tgz -C /var/www/motoma.io; mv /var/www/motoma.io/static/favicon.ico /var/www/motoma.io/favicon.ico; rm static.tgz www.tgz"
