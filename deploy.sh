6f8a4f787c5914f0dcb3642f7503af4eacbc1b6c
Chris Gilbert <Chris-Gilbert@idexx.com>
2012-08-29 13:46:02 -0400

    Added robots.txt

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env sh
+
 tar -czf static.tgz static
+
 cd www
 tar -czf ../www.tgz *
+
 cd ..
 scp *.tgz web1.motoma.io:
-#ssh web1.motoma.io -C "mkdir -p ~/backup; mv /var/www/motoma.io ~/backup/`date -Iminutes`; mkdir -p /var/www/motoma.io; tar -zxvf www.tgz -C /var/www/motoma.io; tar -zxvf static.tgz -C /var/www/motoma.io; mv /var/www/motoma.io/static/favicon.ico /var/www/motoma.io/favicon.ico; rm static.tgz www.tgz"
+rm static.tgz www.tgz
+
 ssh web1.motoma.io -T -C << EOF
 mkdir -p ~/backup
 mv /var/www/motoma.io ~/backup/`date -Iminutes`
@@ -12,7 +16,6 @@ mkdir -p /var/www/motoma.io
 tar -zxf www.tgz -C /var/www/motoma.io
 tar -zxf static.tgz -C /var/www/motoma.io
 mv /var/www/motoma.io/static/favicon.ico /var/www/motoma.io/
+mv /var/www/motoma.io/static/robots.txt /var/www/motoma.io/
 rm static.tgz www.tgz
 EOF
-
-rm static.tgz www.tgz
