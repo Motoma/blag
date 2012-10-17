8b185af6251dde6987849d6b8e5782eb367aee67
Chris Gilbert <Chris-Gilbert@idexx.com>
2012-10-17 10:31:38 -0400

    2012-10-17T10:31-0400 - Automatic commit from build

--- data/post-0021.py
+++ data/post-0021.py
@@ -1,10 +1,10 @@
 author = "Motoma"
 date   = "2011-02-05"
-tags   = "genetic programming, python, ruby"
-title  = "Genetic Programming the Python Way - The Basics"
-url    = "genetic-programming-the-python-way-the-basics"
+tags   = "genetic algorithms, python, ruby"
+title  = "Genetic Algorithms the Python Way - The Basics"
+url    = "genetic-algorithms-the-python-way-the-basics"
 data   = """
-<p>Ages ago, I came across a post by <a href=http://eignerchris.tumblr.com/>Chris Eigner</a> that succinctly delineates the genetic programming method, exemplified with a terse, no-frills example written in <a href=http://www.ruby-lang.org/en/>Ruby</a>. Impressed by the simplicity of his code, I quickly whipped up an implementation in <a href=http://python.org>Python</a>. Weeks turned into months, as I continued to put off posting the code here.</p>
+<p>Ages ago, I came across a post by <a href=http://eignerchris.tumblr.com/>Chris Eigner</a> that succinctly delineates the genetic aglorithm based programming method, exemplified with a terse, no-frills example written in <a href=http://www.ruby-lang.org/en/>Ruby</a>. Impressed by the simplicity of his code, I quickly whipped up an implementation in <a href=http://python.org>Python</a>. Weeks turned into months, as I continued to put off posting the code here.</p>
 
 <!-- break -->
 
