3271d5833cad61e0144cd9ee3c21529e97e04606
Christopher Gilbert <motoma@gmail.com>
2015-08-24 19:58:52 -0400

    Initial Dockerfile

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,13 @@
 #!/usr/bin/env sh
 
-tar -czf static.tgz static
-
-cd www
-tar -czf ../www.tgz *
-
-cd ..
-scp *.tgz web1.motoma.io:
-rm static.tgz www.tgz
-
-ssh web1.motoma.io -T -C << EOF
-mkdir -p ~/backup
-mv /var/www/motoma.io ~/backup/`date -Iminutes`
-mkdir -p /var/www/motoma.io
-tar -zxf www.tgz -C /var/www/motoma.io
-tar -zxf static.tgz -C /var/www/motoma.io
-mv /var/www/motoma.io/static/favicon.ico /var/www/motoma.io/
-mv /var/www/motoma.io/static/robots.txt /var/www/motoma.io/
-mv /var/www/motoma.io/static/motoma.asc /var/www/motoma.io/
-rm static.tgz www.tgz
+for SERVER in p0, p1
+do
+    ssh $SERVER.motoma.io -T -C << EOF
+rm -rf stage && mkdir -p stage && cd stage
+git clone https://github.com/Motoma/blag.git && cd blag
+docker build -t motoma.io .
+docker kill blog
+docker run --name blog -d -p 80:80/tcp -t motoma.io
 EOF
+   
+done
