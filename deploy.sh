23411e6923332f3d44a8a8a4241df75f0975c762
Christopher Gilbert <motoma@gmail.com>
2015-08-24 20:09:11 -0400

    Deploy including proper repository.

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ for SERVER in p0 p1
 do
     ssh $SERVER.motoma.io -T -C << EOF
 rm -rf stage && mkdir -p stage && cd stage
-git clone https://github.com/Motoma/blag.git && cd blag
+git clone https://github.com/Motoma/motoma.io.git && cd motoma.io
 docker build -t motoma.io .
 docker kill blog
 docker run --name blog -d -p 80:80/tcp -t motoma.io
