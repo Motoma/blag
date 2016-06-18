3389ab38c684d1d350ff2493e556787534ca3004
Christopher Gilbert <motoma@gmail.com>
2016-06-17 21:38:50 -0400

    Docker compose.

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,8 @@ do
     ssh $SERVER.motoma.io -T -C << EOF
 rm -rf stage && mkdir -p stage && cd stage
 git clone https://github.com/Motoma/motoma.io.git && cd motoma.io
-docker build -t motoma.io .
-docker kill blog
-docker rm blog
-docker run --name blog -d -p 80:80/tcp -t motoma.io
+docker-compose build
+docker-compose start
 EOF
    
 done
